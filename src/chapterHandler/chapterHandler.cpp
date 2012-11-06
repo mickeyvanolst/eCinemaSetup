55d0b624c17ddb75e5804cc7907288acb47fba0f
Mick van Olst <mick@popsure.nl>
2012-11-06 13:16:17 +0100

    created  chapterHandler
    created a nice class that reads out the directory, finds all
    chapter-folders then checks if the L, M, R video files are there, then
    things as dimensions and filesize are being written to a double-vector
    object. Also it gets written into an XML file.

---
+++ src/chapterHandler/chapterHandler.cpp
@@ -0,0 +1,9 @@
+//
+//  chapterHandler.cpp
+//  eCinemaSetup
+//
+//  Created by Mick van Olst on 05-11-12.
+//
+//
+
+#include "chapterHandler.h"
---
+++ src/chapterHandler/chapterHandler.h
@@ -0,0 +1,14 @@
+//
+//  chapterHandler.h
+//  eCinemaSetup
+//
+//  Created by Mick van Olst on 05-11-12.
+//
+//
+
+#ifndef eCinemaSetup_chapterHandler_h
+#define eCinemaSetup_chapterHandler_h
+
+
+
+#endif
